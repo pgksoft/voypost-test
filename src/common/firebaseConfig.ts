97cedcc9998a6938584c068fd012269e92cc1531
pgksoft <pgksoft@ukr.net>
2022-06-14 10:00:30 +0300

    In the file src/common/firebaseConfig.ts replaced the firebaseConfig value with the config of the web app my created inside my firebase Project

--- src/common/firebaseConfig.ts
+++ src/common/firebaseConfig.ts
@@ -1,12 +1,14 @@
 // eslint-disable-next-line import/no-mutable-exports, @typescript-eslint/ban-types
 const firebaseConfig: Object = {
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  projectId: 'xxx',
-  storageBucket: 'xxx',
-  messagingSenderId: 'xxx',
-  appId: 'xxx',
-  measurementId: 'xxx',
+  apiKey: 'AIzaSyD5a6gTds1F0QPuo1oGcojHr-iJ5bLTe-c',
+  authDomain: 'pgksoft-web-test-from-voypost.firebaseapp.com',
+  databaseURL:
+    'https://pgksoft-web-test-from-voypost-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'pgksoft-web-test-from-voypost',
+  storageBucket: 'pgksoft-web-test-from-voypost.appspot.com',
+  messagingSenderId: '442080140853',
+  appId: '1:442080140853:web:b7523fc24295f9c2b19b7c',
+  measurementId: 'G-BJYC0C4M90',
 };
 
 export default firebaseConfig;
