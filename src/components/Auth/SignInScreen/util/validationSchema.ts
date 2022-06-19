d927691a7d33cc5f5a68a4a5bba24040b76732d6
pgksoft <pgksoft@ukr.net>
2022-06-19 17:16:50 +0300

    fix VasylHarastei: [maint] схемы валидации нужно перенести в соотв папки компонент, v-malashevskyi: тут лучше продублировать, схема не может быть внутри Auth, потому что Auth - это домен, а в домене могут быть только компоненты

--- src/components/Auth/SignInScreen/util/validationSchema.ts
+++ src/components/Auth/SignInScreen/util/validationSchema.ts
@@ -1,9 +1,21 @@
-import { object } from 'yup';
-import {
-  emailValidationSchema,
-  passwordValidationSchema,
-} from '../../FieldValidationSchemes';
+import { object, string } from 'yup';
 import { keySingInFormValues } from './values';
+
+const MIN_PASSWORD_LENGTH = 12;
+
+const TITLES = {
+  stringValidateEmail: 'Invalid email address',
+  stringValidateEmpty: 'Required',
+  stringValidateMin: `Must be ${MIN_PASSWORD_LENGTH} characters or more`,
+};
+
+export const emailValidationSchema = string()
+  .email(TITLES.stringValidateEmail)
+  .required(TITLES.stringValidateEmpty);
+
+const passwordValidationSchema = string()
+  .min(MIN_PASSWORD_LENGTH, TITLES.stringValidateMin)
+  .required(TITLES.stringValidateEmpty);
 
 const validationSchema = object({
   [keySingInFormValues.email]: emailValidationSchema,
--- src/components/Auth/SignUpScreen/util/validationSchema.ts
+++ src/components/Auth/SignUpScreen/util/validationSchema.ts
@@ -1,16 +1,43 @@
-import { object } from 'yup';
-import {
-  emailValidationSchema,
-  fullNameValidationSchema,
-  passwordValidationSchema,
-} from '../../FieldValidationSchemes';
+import { object, string, ref } from 'yup';
 import { keySingUpFormValues } from './values';
+
+const MIN_PASSWORD_LENGTH = 12;
+
+const TITLES = {
+  stringValidateEmail: 'Invalid email address',
+  stringValidateEmpty: 'Required',
+  stringValidateMin: `Must be ${MIN_PASSWORD_LENGTH} characters or more`,
+  stringValidateFullName:
+    'Name field has to contain at least 2 words, each of which has to start from a capital letter',
+  notMatchPasswords: "Passwords don't match!",
+};
+
+const emailValidationSchema = string()
+  .email(TITLES.stringValidateEmail)
+  .required(TITLES.stringValidateEmpty);
+
+const passwordValidationSchema = string()
+  .min(MIN_PASSWORD_LENGTH, TITLES.stringValidateMin)
+  .required(TITLES.stringValidateEmpty);
+
+const fullNameValidationSchema = string()
+  .required(TITLES.stringValidateEmpty)
+  .matches(
+    /^[A-Z][a-z]+[,.'-]?(?: [A-Z][a-z']+[,.'-]?){1,}$/,
+    TITLES.stringValidateFullName,
+  )
+  .trim('spaces');
+
+const requestPasswordValidationSchema = string()
+  .oneOf([ref(keySingUpFormValues.password), null], TITLES.notMatchPasswords)
+  .min(MIN_PASSWORD_LENGTH, TITLES.stringValidateMin)
+  .required(TITLES.stringValidateEmpty);
 
 const validationSchema = object({
   [keySingUpFormValues.email]: emailValidationSchema,
   [keySingUpFormValues.fullName]: fullNameValidationSchema,
   [keySingUpFormValues.password]: passwordValidationSchema,
-  [keySingUpFormValues.requestPassword]: passwordValidationSchema,
+  [keySingUpFormValues.requestPassword]: requestPasswordValidationSchema,
 });
 
 export default validationSchema;
